15b8517522d690614ac2f3e8671b8dd68381ef0e
Addie-020 <shenzhiyu111i@126.com>
2022-09-18 15:49:20 +0800

    New update

--- 3_JointEstimationTest/joint_estimator_improved/ConjGradeOptim.m
+++ 3_JointEstimationTest/joint_estimator_improved/ConjGradeOptim.m
@@ -1,11 +1,11 @@
 function [xBest, yBest, info] = ConjGradeOptim(x0, nvars, Ct, Fs, options)
- 
+
 %
 % Conjugate Gradient Algorithm (with Polak-Ribiere method)
 % i.e. PR-CG algorithm
 % Adopt golden section method as linear search optimization algorithm
 % Input vector x0: 1*nvars
-% 
+%
 % Input arguments:
 %   @x0     : Initial value of variables
 %   @nvars  : Number of variables
@@ -39,62 +39,39 @@ else
 end % end: if
 options = SetOptions(userOptions, nvars, Ct, Fs);
 
-% Assign some paramters
-delta = options.delta;
-
 
 %%% Initialization
 
-% Calculate function value of initial point
-xval = x0;
-fval = ObjFun(xval, Ct, Fs);
+% Create initial state: function value, gradient value, iteration direction
+state = MakeState(x0, nvars, options);
 
-% Calculate partial differential
-xDel = diag(delta * ones(D, 1));                % D * D matrix
-xInc = repmat(xval, 1, D) + xDel;           % D * D matrix
-funValInc = ObjFun(xInc, Ct, Fs);           % 1 * D matrix
-gVal0 = ((funValInc - fval * ones(1, D)) / delta).';
-gVal = gVal0;
-dVal = zeros(D, 1);
+%%% Iteration
 
-%%% Memory Allocation
+exitFlag = [];
+while isempty(exitFlag)
 
-% Allocate memory for info
-info.freqVal        = zeros(1, maxIter);        % Frequency value of current iteration
-info.phaVal         = zeros(1, maxIter);        % Phase value of current iteration
-info.funVal         = zeros(1, maxIter);        % Objective function value of current iteration
-info.gradFreq       = zeros(1, maxIter);        % Frequency component of gradient of current iteration
-info.gradPha        = zeros(1, maxIter);        % Phase component of gradient of current iteration
-info.iter           = 1 : maxIter;
+    % Calculate step length with Golden Section method
+    state = GoldenSection(state, options);
 
+    % Update state
 
-%%% Main Loop
+    % Update search direction
 
-iter = 1;
-while (max(abs(gVal)) > epsilon) && (iter <= maxIter)
-    
     % Using PR method to calculate step length of direction updating
     bValTemp = (gVal' * (gVal - gVal0)) / (norm(gVal0))^2;
     bVal = max(bValTemp, 0);
 
     % Update 2D search direction
-    dVal = -gVal + bVal .* dVal;
+    iterDirecrion = -gVal + bVal .* iterDirecrion;
 
     % Optimize search step with linear search optimization algorithm
-    [xval, fval] = StepOptim(xval, dVal, stepErr, stepDist, Ct, Fs);
-    
+    [xval, fval] = GoldenSection(xval, iterDirecrion, stepErr, stepDist, Ct, Fs);
+
     % Calculate partial differential
     xInc = repmat(xval, 1, D) + xDel;
     funValInc = ObjFun(xInc, Ct, Fs);
     gVal0 = gVal;
     gVal = ((funValInc - fval * ones(1, D)) / delta).';
-    
-    % Log Data
-    info.freqVal(iter)  = xval(1);
-    info.phaVal(iter)   = xval(2);
-    info.funVal(iter)   = fval;
-    info.gradFreq(iter) = abs(gVal(1));
-    info.gradPha(iter)  = abs(gVal(2));
 
     % Print
     if strcmp('iter', options.display)
@@ -105,7 +82,7 @@ while (max(abs(gVal)) > epsilon) && (iter <= maxIter)
                 info.gradFreq(iter), info.gradPha(iter));
         end
     end
-    
+
     % Update iteration time
     iter = iter + 1;
 
@@ -135,12 +112,13 @@ function options = SetOptions(userOptions, nvars, Ct, Fs)
 %
 
 % Option Defult Set
-default.delta              = 1e-9;                  % Infinitesimal for calculating gradient
+default.deltaFreq          = 1e-9;                  % Frequency infinitesimal for calculating gradient
+default.deltaPha           = 1e-9;                  % Phase infinitesimal for calculating gradient
 default.tolGradValue       = 1e-9;                  % Termination tolerance on gradient value
 default.maxIteration       = min(100, 200*nvars);   % Maximum iteration times
 default.maxTime            = inf;                   % Maximum time the algorithm runs
 default.initialStepLength  = 0.6;                   % Initial step length for multidimension search
-default.stepSearchDistance = 1e-9;                  % Search distance when calculating step length
+default.stepOptimDistance  = 1e-9;                  % Search distance when calculating step length
 default.tolStepError       = 1e-9;                  % Termination tolerance on function value calculating step length
 default.display            = 'none';                % Whether iteration progress printed on the screen
 default.displayInterval    = 1;                     % Iteration interval printed on the screen
@@ -176,6 +154,7 @@ function state = MakeState(x0, nvars, options)
 % Create an initial set of particles and objective function values
 %
 % Input arguments:
+%   @x0     : Initial variable value
 %   @nvars  : Number of variables
 %   @options: Optimization options
 %
@@ -185,20 +164,22 @@ function state = MakeState(x0, nvars, options)
 
 % A variety of data used in various places
 state = struct;
-state.xVal = x0;                    % Initial variable value
+state.varVal = x0;                  % Initial variable value
 state.iteration = 0;                % Current generation counter
 state.startTime = tic;              % Tic identifier
 state.stopFlag = false;             % OutputFcns flag to end the optimization
 
-% Calculate the objective function value for all particles.
+% Calculate the objective function value for initial point
 Ct = options.covarianceMatrix;
 Fs = options.samplingFrequency;
-fvals = ObjFun(state.positions, Ct, Fs);
-state.fvals = fvals;
-state.funEval = numParticles;
+funVal = ObjFun(state.varVal, Ct, Fs);
+state.funVal = funVal;
 
-state.individualBestFvals = state.fvals;
-state.individualBestPositions = state.positions;
+% Calculate initial objective function gradient
+state.gradVal = GradientCompute(state, options, nvars);
+
+% Set initial search direction
+state.direction = -state.gradVal;
 
 end % end: function MakeState
 
@@ -206,53 +187,40 @@ end % end: function MakeState
 
 %%%% Function "GradientCompute"
 
-function options = GradientCompute(options, nvars, xval)
+function gradVal = GradientCompute(state, options, nvars)
 %
 % Compute gradient of function by calculating numerical differential
 %
 % Input arguments:
+%   @state  : State struct of iteration
 %   @options: Optimization options
 %   @nvars  : Number of variables
 %   @xval   : Variable value
 %
 % Output arguments:
-%   @options: Options for optimization
+%   @gradVal: Gradient value
 %
 
-% Calculate partial differential
+deltaMat = [options.deltaFreq, options.deltaPha];
+newVarVal = repmat(state.varVal, nvars, 1) + diag(deltaMat);
+gradVal = (newVarVal - state.funVal) ./ deltaMat.';
+
+end % end: function GradientCompute
 
 
 
+%%%% function "GoldenSection"
 
-
-xDel = diag(delta * ones(D, 1));                % D * D matrix
-xInc = repmat(xval, 1, D) + xDel;           % D * D matrix
-funValInc = ObjFun(xInc, Ct, Fs);           % 1 * D matrix
-gVal0 = ((funValInc - fval * ones(1, D)) / delta).';
-gVal = gVal0;
-
-
-end
-
-
-
-%%%% function "StepOptim"
-
-function [xBest, yBest] = StepOptim(x0, d0, epsilon, dist, Ct, Fs)
+function state = GoldenSection(state, options)
 
 %
 % Linear search optimization algorithm
 % Optimize the step of 2D search
 % Adopt golden section method
-% 
+%
 % Input arguments:
-%   @x0     : Initial variable value
-%   @g0     : Initial gradient value
-%   @funVal0: Initial function value
-%   @epsilon: Exit flag for search
-%   @dist   : Search distance
-%   @Ct     : Necessary information of sequence to be estimated
-%   @Fs     : Sampling rate
+%   @state  : State struct of iteration
+%   @options: Optimization options
 %
 % Output arguments:
 %   @xBest  : Optimal point (variable)
@@ -262,87 +230,145 @@ function [xBest, yBest] = StepOptim(x0, d0, epsilon, dist, Ct, Fs)
 %%% Initialization
 
 % Calculate golden section
-w1 = (sqrt(5) - 1) / 2;     % 0.618
-w2 = (3 - sqrt(5)) / 2;     % 0.382
+w1 = (sqrt(5)-1) / 2;       % 0.618
+w2 = (3-sqrt(5)) / 2;       % 0.382
 
-% Set initial scale
-a = 0;                      % Upper bound
-b = dist;                   % Lower bound
+% Assign value of initial point
+x0 = state.varVal;
+d0 = state.direction;
+Ct = options.covarianceMatrix;
+Fs = options.samplingFrequency;
 
-% Set initial middle points
-c = a + w1 * (b - a);
-d = a + w2 * (b - a);
+% Set initial search scale
+a = 0;
+b = options.stepOptimDistance;
+
+% Set initial middle points of search
+c = a + w1*(b-a);
+d = a + w2*(b-a);
 
 % Calculate function value of middle points
-xc = x0 + c * d0;
+xc = x0 + c*d0;
 fc = ObjFun(xc, Ct, Fs);
-xd = x0 + d * d0;
+xd = x0 + d*d0;
 fd = ObjFun(xd, Ct, Fs);
 
 
 %%% Iteration
 
 iter = 0;
-while abs(b - a) > epsilon
-    
+while abs(b-a) > state.tolStepError
+
     % Update points
     if fc > fd
         % Update end points
         b = d;
         % Update end middle points
         d = c;
-        c = a + w1 * (b - a);
+        c = a + w1*(b-a);
         % Update function values
         fd = fc;
-        xc = x0 + c * d0;
+        xc = x0 + c*d0;
         fc = ObjFun(xc, Ct, Fs);
     elseif fc < fd
         % Update end points
         a = c;
         % Update end middle points
         c = d;
-        d = a + w2 * (b - a);
+        d = a + w2*(b-a);
         % Update function values
         fc = fd;
-        xd = x0 + d * d0;
+        xd = x0 + d*d0;
         fd = ObjFun(xd, Ct, Fs);
     else
         % Update end points
         a = c;
         b = d;
         % Update end middle points
-        c = a + w1 * (b - a);
-        d = a + w2 * (b - a);
+        c = a + w1*(b-a);
+        d = a + w2*(b-a);
         % Update function values
-        xc = x0 + c * d0;
+        xc = x0 + c*d0;
         fc = ObjFun(xc, Ct, Fs);
-        xd = x0 + d * d0;
+        xd = x0 + d*d0;
         fd = ObjFun(xd, Ct, Fs);
-    end 
+    end % end: if
 
     % Update iteration time
     iter = iter + 1;
 
-end
+end % end: for
 
 %%% Set output value
 
 % Calculate function value of endpoints
-xa = x0 + a * d0;
+xa = x0 + a*d0;
 fa = ObjFun(xa, Ct, Fs);
-xb = x0 + b * d0;
-fb = ObjFun(xb, Ct, Fs);
+xb = x0 + b*d0;
+fb = ObjFun(b, Ct, Fs);
 
 % Compare function value of end points and determine output value
 if fa > fb
-    yBest = fa;
-    xBest = xa;
+    state.funVal = fa;
+    state.varVal = xa;
 else
-    yBest = fb;
-    xBest = xb;
-end
+    state.funVal = fb;
+    state.varVal = xb;
+end % end: if
 
-end
+end % end: function GoldenSection
+
+
+
+%%%% Function "UpdateState"
+
+function state = UpdateState(state, numParticles, pIdx)
+% 
+% Update best fvals and best individual positions
+%
+% Input arguments:
+%   @state       : State struct
+%   @numParticles: Number of particles
+%   @pIdx        : Particles index
+%
+% Output arguments:
+%   @state: State struct
+%
+
+state.funEval = state.funEval + numel(pIdx);
+
+% Remember the best fvals and positions for this block
+tfImproved = false(numParticles, 1);
+tfImproved(pIdx) = state.fvals(pIdx) < state.individualBestFvals(pIdx);
+state.individualBestFvals(tfImproved) = state.fvals(tfImproved);
+state.individualBestPositions(tfImproved, :) = state.positions(tfImproved, :);
+
+end % end: function UpdateState
+
+
+
+%%%% Function "UpdateDirection"
+
+function newDirection = UpdateDirection(state, nvars)
+%
+% Update the velocities of particles with indices pIdx
+%
+% Input arguments:
+%   @state            : State struct of optimization
+%   @nvars            : Number of variables
+%
+% Output arguments:
+%   @newDirection: Updated iteration direction
+%
+
+% Using PR method to calculate step length of direction updating
+bValTemp = (gVal' * (gVal - gVal0)) / (norm(gVal0))^2;
+bVal = max(bValTemp, 0);
+
+% Update 2D search direction
+iterDirecrion = -gVal + bVal .* iterDirecrion;
+
+end % end: function UpdateVelocities
 
 
 
