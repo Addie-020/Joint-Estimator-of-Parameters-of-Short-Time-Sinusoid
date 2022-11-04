77ab177ccd018859708c6b8ede15684f90626d92
Zhiyu Shen <shenzhiyu111i@126.com>
2022-11-04 11:06:03 +0800

    New update

--- 3_JointEstimationTest/method_comparison/CompareLoopSnr.m
+++ 3_JointEstimationTest/method_comparison/CompareLoopSnr.m
@@ -31,12 +31,16 @@ if isempty(Fs)
     Fs = 10;
 end
 Tt = 0.5 / ft;                      % Total time of sampling (s)
+Tt1 = 5 / ft;
 Ns = round(Tt * Fs);                % Total sampling points
+Ns1 = round(Tt1 * Fs);
 
 % Generate original signal sequence
 xt = (0 : Ns - 1) / Fs;             % Time index
+xt1 = (0 : Ns - 1) / Fs;
 at = 1;                             % Signal amplitude
 xn0 = at * cos(2*pi*ft*xt + pt);    % Test signal
+xn1 = at * cos(2*pi*ft*xt1 + pt);
 
 % Define estimator options
 maxIter = 10;                       % Maximum iteration time for each estimation
@@ -53,9 +57,13 @@ phaMseA = zeros(1, numSnr);         % MSE of phase (Joint)
 timeMeanA = zeros(1, numSnr);       % Mean of time (Joint)
 timeVarA = zeros(1, numSnr);        % Variance of time (Joint)
 freqMseB = zeros(1, numSnr);        % MSE of frequency (Peak)
-phaMseb = zeros(1, numSnr);         % MSE of phase (Peak)
+phaMseB = zeros(1, numSnr);         % MSE of phase (Peak)
 timeMeanB = zeros(1, numSnr);       % Mean of time (Peak)
 timeVarB = zeros(1, numSnr);        % Variance of time (Peak)
+freqMseC = zeros(1, numSnr);        % MSE of frequency (Phase)
+phaMseC = zeros(1, numSnr);         % MSE of phase (Phase)
+timeMeanC = zeros(1, numSnr);       % Mean of time (Phase)
+timeVarC = zeros(1, numSnr);        % Variance of time (Phase)
 
 poolobj = parpool(12);
 parfor i = 1 : numSnr
@@ -63,13 +71,18 @@ parfor i = 1 : numSnr
     % Add noise
     sigNoise = sigmaN(i)*randn(1, Ns);      % Additive white Gaussian noise
     xn = xn0 + sigNoise;
+    xn2 = xn1 + sigNoise;
     
     % Estimate with Joint Estimator
-    [freqMseA(i), phaMseA(i), timeMeanA(i), timeVarA(i)] = JointEstimatorTest(xn, ft, pt, Fs, ...
-        Tt, numEst, maxIter);
+    [freqMseA(i), phaMseA(i), timeMeanA(i), timeVarA(i)] = JointEstimatorTest(xn, ...
+        ft, pt, Fs, Tt, numEst, maxIter);
     % Estimate with DTFT Peak Search
-    [freqMseB(i), phaMseB(i), timeMeanB(i), timeVarB(i)] = PeakSearchTest(xn, ...
-    ft, pt, Fs, 4*Tt, numEst)
+    [freqMseB(i), phaMseB(i), timeMeanB(i), timeVarB(i)] = PeakSearchTest(xn2, ...
+    ft, pt, Fs, Tt1, numEst)
+    % Estimate with Phase Difference Method
+    [freqMseC(i), phaMseC(i), timeMeanC(i), timeVarC(i)] = PhaseDiffTest(xn2, ...
+    ft, pt, Fs, Tt1, numEst)
+
 
     fprintf('Estimation No.%d, SNR = %.1f\n', i, snrSig(i));
 
@@ -91,11 +104,12 @@ timePlt.WindowState = 'maximized';
 hold on
 plot(snrSig, timeMeanA, 'LineWidth', 2, 'Color', '#0072BD', 'Marker', '*', 'MarkerSize', 8);
 plot(snrSig, timeMeanB, 'LineWidth', 2, 'Color', '#D95319', 'Marker', '*', 'MarkerSize', 8);
+plot(snrSig, timeMeanC, 'LineWidth', 2, 'Color', '#77AC30', 'Marker', '*', 'MarkerSize', 8);
 hold off
 % Set the plotting properties
 xlabel("SNR (dB)", "Interpreter", "latex");
 ylabel("Mean Estimation Time (s)", "Interpreter", "latex");
-legend('Joint Estimator', 'Peak Search');
+legend('Joint Estimator', 'Peak Search', 'Phase Difference');
 set(gca, 'Fontsize', 20);
 
 % Plot relationship between MSE and SNR
@@ -107,22 +121,24 @@ subplot(2, 1, 1);
 hold on
 plot(snrSig, log10(freqMseA), 'LineWidth', 2, 'Color', '#0072BD', 'Marker', '*', 'MarkerSize', 8);
 plot(snrSig, log10(freqMseB), 'LineWidth', 2, 'Color', '#D95319', 'Marker', '+', 'MarkerSize', 8);
-plot(snrSig, log10(varLbFreq), 'LineWidth', 2, 'Color', '#77AC30', 'Marker', 'o', 'MarkerSize', 8);
+plot(snrSig, log10(freqMseC), 'LineWidth', 2, 'Color', '#77AC30', 'Marker', '+', 'MarkerSize', 8);
+plot(snrSig, log10(varLbFreq), 'LineWidth', 2, 'Color', '#A2142F', 'Marker', 'o', 'MarkerSize', 8);
 hold off
 xlabel("SNR (dB)", "Interpreter", "latex");
 ylabel("$\log_{10}(MSE_{frequency})$", "Interpreter", "latex");
-legend('Joint Estimator', 'Peak Search', 'CRLB');
+legend('Joint Estimator', 'Peak Search', 'Phase Difference', 'CRLB');
 set(gca, 'Fontsize', 20);
 % Plot phase MSE-SNR curve
 subplot(2, 1, 2);
 hold on
 plot(snrSig, log10(phaMseA), 'LineWidth', 2, 'Color', '#0072BD', 'Marker', '*', 'MarkerSize', 8);
 plot(snrSig, log10(phaMseB), 'LineWidth', 2, 'Color', '#D95319', 'Marker', '+', 'MarkerSize', 8);
-plot(snrSig, log10(varLbPha), 'LineWidth', 2, 'Color', '#77AC30', 'Marker', 'o', 'MarkerSize', 8);
+plot(snrSig, log10(phaMseC), 'LineWidth', 2, 'Color', '#77AC30', 'Marker', '+', 'MarkerSize', 8);
+plot(snrSig, log10(varLbPha), 'LineWidth', 2, 'Color', '#A2142F', 'Marker', 'o', 'MarkerSize', 8);
 hold off
 xlabel("SNR (dB)", "Interpreter", "latex");
 ylabel("$\log_{10}(MSE_{phase})$", "Interpreter", "latex");
-legend('Joint Estimator', 'Peak Search', 'CRLB');
+legend('Joint Estimator', 'Peak Search', 'Phase Difference', 'CRLB');
 set(gca, 'Fontsize', 20);
 
 
